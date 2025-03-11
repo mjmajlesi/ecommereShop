268cf0c609b6cd00b22c258b36b6a40983c605a2
mohammadjavadmajlesi <mohammadjavadmajlesi313@gmail.com>
2025-03-11 08:06:47 +0330

    add Svgs and import to project with category.ts

---
+++ src/Database/Category.ts
@@ -0,0 +1,57 @@
+import Laptop from "../../public/images/laptop.svg";
+import Camera from "../../public/images/camera.svg";
+import Phone from "../../public/images/smartphone.svg";
+import Watch from "../../public/images/smartwatch.svg";
+import Headphone from "../../public/images/headphones.svg";
+import Game from "../../public/images/gamepad.svg";
+import Computer from "../../public/images/computer.svg";
+import Flash from "../../public/images/flash drive.svg";
+import Tablet from "../../public/images/tablet.svg";
+
+export const Dcategory = [
+  {
+    id: 1,
+    name: "Laptops",
+    image: Laptop,
+  },
+  {
+    id: 2,
+    name: "Camera & Photos",
+    image: Camera,
+  },
+  {
+    id: 3,
+    name: "MobilePhones",
+    image: Phone,
+  },
+  {
+    id: 4,
+    name: "SmartWatchs",
+    image: Watch,
+  },
+  {
+    id: 5,
+    name: "Headphones",
+    image: Headphone,
+  },
+  {
+    id: 6,
+    name: "GameConsoles & Devices",
+    image: Game,
+  },
+  {
+    id: 7,
+    name: "Computer devices",
+    image: Computer,
+  },
+  {
+    id: 8,
+    name: "FlashDrives",
+    image: Flash,
+  },
+  {
+    id: 9,
+    name: "Tablet & e-bookReaders",
+    image: Tablet,
+  }
+];
