7271b7a4b084c1a63ae6c0c2dbb0221a0ad2e381
mohammadjavadmajlesi <mohammadjavadmajlesi313@gmail.com>
2025-03-13 16:39:01 +0330

    add Deals component and Timer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Category from "@/Components/Categorys";
+import Deals from "@/Components/Deals";
 import Header from "@/Components/Header";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <Header />
       <Category />
+      <Deals />
     </div>
   );
 }
