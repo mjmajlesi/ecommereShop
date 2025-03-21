9d3ed85190294a15433939ba9458db100ed70d9b
mohammadjavadmajlesi <mohammadjavadmajlesi313@gmail.com>
2025-03-21 20:53:46 +0330

    complete footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { FaGithub, FaLinkedinIn, FaTelegramPlane } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>
-        <h1>Footer</h1>
+    <div className="w-full bg-[#292927] text-white p-4 px-6 mt-12 flex items-center justify-between ">
+      <span className="font-semibold ">
+        © 2024 Copyright: Mohammad Javad Majlesi
+      </span>
+      <div className="flex items-center gap-3">
+        <a href="https://github.com/mjmajlesi" target="_blank">
+          <FaGithub size={20} />
+        </a>
+        <a href="https://t.me/Mj_majlesi" target="_blank">
+          <FaTelegramPlane size={20} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mohammad-javad-majlesi-0ab27331a/"
+          target="_blank"
+        >
+          <FaLinkedinIn size={20} />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
