bde6ad418745cfa6d3fdc6a2230e2ef620e61806
mohammadjavadmajlesi <mohammadjavadmajlesi313@gmail.com>
2025-03-26 02:27:05 +0330

    create and add swaperjs to store page and complete store page

---
+++ src/Components/Swipe.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay , EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import Image from "next/image";
+import { imagesforswaper } from "../../lib/image";
+
+function Swipe() {
+  return (
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={false}
+      effect={'fade'}
+      modules={[Autoplay, Pagination, Navigation , EffectFade]}
+      className="w-full rounded-lg h-[300px]"
+    >
+        {
+            imagesforswaper.map((image , index) => (
+                <SwiperSlide key={index}>
+                    <Image src={image.src} alt={image.alt} className="w-full" />
+                </SwiperSlide>
+            ))
+        }
+    </Swiper>
+  );
+}
+
+export default Swipe;
--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,6 +1,9 @@
 import Category, { Icategory } from "@/Components/Category";
 import Container from "@/Components/Container";
+import Swipe from "@/Components/Swipe";
 import React from "react";
+import { imageBrands } from "../../../lib/image";
+import Image from "next/image";
 
 async function Store() {
   const results = await fetch("http://localhost:3001/products");
@@ -9,11 +12,30 @@ async function Store() {
   return (
     <div>
       <Container>
-      <div className="grid grid-cols-4 gap-3">
-        {category.map((items) => (
-          <Category key={items.id} {...items} />
-        ))}
-      </div>
+        <div className="flex flex-col items-center justify-between gap-8 my-6">
+          <Swipe />
+          <div className="flex flex-col items-center justify-center my-8">
+            <span className="font-semibold text-4xl p-3 mb-4 ">Brands</span>
+            <div className="grid grid-cols-7 gap-10">
+              {imageBrands.map((brand, index) => (
+                <Image
+                  key={index}
+                  src={brand.src}
+                  alt={brand.alt}
+                  className="w-[140px]"
+                />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center my-8">
+            <span className="font-semibold text-4xl p-3 mb-4 ">Category</span>
+            <div className="grid grid-cols-5 gap-8">
+              {category.map((items) => (
+                <Category key={items.id} {...items} />
+              ))}
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
