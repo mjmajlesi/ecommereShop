ae3dde313b0578d33f9e18a5662f978873ebd9ad
mohammadjavadmajlesi <mohammadjavadmajlesi313@gmail.com>
2025-03-26 02:28:41 +0330

    add images and brands and better design folder image

---
+++ lib/image.ts
@@ -0,0 +1,61 @@
+import Laptop from "../public/images/swaper/macbook.jpg";
+import tablet from "../public/images/swaper/iPad.jpg";
+import phone from "../public/images/swaper/phone.png";
+import phone2 from "../public/images/swaper/phone2.png";
+import apple from "../public/images/Brands/apple.svg";
+import Google from "../public/images/Brands/google.svg";
+import asus from "../public/images/Brands/asus.svg";
+import dell from "../public/images/Brands/dell.svg";
+import lenovo from "../public/images/Brands/lenovo.svg";
+import microsoft from "../public/images/Brands/microsoft.svg";
+import samsung from "../public/images/Brands/samsung.svg";
+
+export const imagesforswaper = [
+  {
+    src: Laptop,
+    alt: "Laptop",
+  },
+  {
+    src: tablet,
+    alt: "tablet",
+  },
+  {
+    src: phone,
+    alt: "phone",
+  },
+  {
+    src: phone2,
+    alt: "phone2",
+  },
+];
+
+export const imageBrands = [
+  {
+    src : apple,
+    alt : "apple"
+  },
+  {
+    src : Google,
+    alt : "Google"
+  },
+  {
+    src : asus,
+    alt : "asus"
+  },
+  {
+    src : dell,
+    alt : "dell"
+  },
+  {
+    src : lenovo,
+    alt : "lenovo"
+  },
+  {
+    src : microsoft,
+    alt : "microsoft"
+  },
+  {
+    src : samsung,
+    alt : "samsung"
+  },
+]
